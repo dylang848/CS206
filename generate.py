25e9a2eb51d4805e910ac4b3cb6edfa336411823
dylang848 <57970441+dylang848@users.noreply.github.com>
2022-01-31 12:39:44 -0500

    joints push

--- generate.py
+++ generate.py
@@ -8,19 +8,9 @@ x = 0
 y = 0
 z = 0.5
 
-pyrosim.Start_SDF("boxes.sdf")
+pyrosim.Start_SDF("world.sdf")
+pyrosim.Send_Cube(name="Box", pos=[x, y, z] , size=[length, width, height])
 
-for x in range(0, 8):
-    for y in range(0,8):
-        z=0
-        length = 1
-        width = 1
-        height = 1
-        for z in range(0,8):
-            pyrosim.Send_Cube(name="Box", pos=[x, y, z+.5] , size=[length, width, height])
-            length = .9*length
-            width = .9*width
-            height = .9*height
 
 
 
--- simulate.py
+++ simulate.py
@@ -8,7 +8,7 @@ physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
-p.loadSDF("boxes.sdf")
+p.loadSDF("world.sdf")
 for i in range(1000):
     time.sleep(1/60)
     p.stepSimulation()

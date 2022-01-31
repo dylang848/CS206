c2a1512d731413d6c4457b31ef795b9de0ccaf60
dylang848 <57970441+dylang848@users.noreply.github.com>
2022-01-30 21:24:55 -0500

    a buncha boxes

--- generate.py
+++ generate.py
@@ -6,10 +6,25 @@ height = 1
 
 x = 0
 y = 0
-z = .5
+z = 0.5
 
 pyrosim.Start_SDF("boxes.sdf")
-pyrosim.Send_Cube(name="Box", pos=[x, y, z] , size=[length, width, height])
-pyrosim.Send_Cube(name="Box2", pos=[0, 1, 1.5] , size=[length, width, height])
+
+for x in range(0, 8):
+    for y in range(0,8):
+        z=0
+        length = 1
+        width = 1
+        height = 1
+        for z in range(0,8):
+            pyrosim.Send_Cube(name="Box", pos=[x, y, z+.5] , size=[length, width, height])
+            length = .9*length
+            width = .9*width
+            height = .9*height
+
+
+
+
+
 
 pyrosim.End()

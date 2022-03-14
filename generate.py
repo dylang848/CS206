3463307da9c63a5a8c645f7bdcc7cb170a9604ff
dylang848 <57970441+dylang848@users.noreply.github.com>
2022-03-14 15:56:57 -0400

    random seARCH complete

--- generate.py
+++ generate.py
@@ -1,4 +1,5 @@
 import pyrosim.pyrosim as pyrosim
+import random
 
 length = 1
 width = 1
@@ -29,10 +30,18 @@ def Generate_Brain():
     pyrosim.Send_Sensor_Neuron(name=2, linkName='FrontLeg')
     pyrosim.Send_Motor_Neuron(name=3, jointName='Torso_BackLeg')
     pyrosim.Send_Motor_Neuron(name=4, jointName='Torso_FrontLeg')
-    pyrosim.Send_Synapse(sourceNeuronName=0, targetNeuronName=3, weight=-1.5)
-    pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=3, weight=0.5)
-    pyrosim.Send_Synapse(sourceNeuronName=0, targetNeuronName=4, weight=0.5)
-    pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=4, weight=1.5)
+
+    for i in range(3):
+        for q in range(3,5):
+            pyrosim.Send_Synapse(sourceNeuronName=i, targetNeuronName=q, weight= random.uniform(-1,1))
+
+
+
+
+    #pyrosim.Send_Synapse(sourceNeuronName=0, targetNeuronName=3, weight=-1.5)
+    #pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=3, weight=0.5)
+    #pyrosim.Send_Synapse(sourceNeuronName=0, targetNeuronName=4, weight=0.5)
+    #pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=4, weight=1.5)
 
     pyrosim.End()
 

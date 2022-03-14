cd52905b8ec266c041a2dc395b6f8bee30e1628e
dylang848 <57970441+dylang848@users.noreply.github.com>
2022-03-14 16:16:16 -0400

    creating the hill climber

--- generate.py
+++ generate.py
@@ -37,12 +37,6 @@ def Generate_Brain():
 
 
 
-
-    #pyrosim.Send_Synapse(sourceNeuronName=0, targetNeuronName=3, weight=-1.5)
-    #pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=3, weight=0.5)
-    #pyrosim.Send_Synapse(sourceNeuronName=0, targetNeuronName=4, weight=0.5)
-    #pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=4, weight=1.5)
-
     pyrosim.End()
 
 

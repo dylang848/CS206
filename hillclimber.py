1052ef83689986764dfd578c53cd1d2f438723c4
dylang848 <57970441+dylang848@users.noreply.github.com>
2022-03-14 23:00:10 -0400

    finish hillclimber

--- hillclimber.py
+++ hillclimber.py
@@ -1,6 +1,7 @@
 import copy
 from solution import SOLUTION
 import constants as c
+import os
 
 class HILL_CLIMBER:
     def __init__(self):
@@ -8,20 +9,20 @@ class HILL_CLIMBER:
 
 
     def Evolve(self):
-        self.parent.Evaluate()
+        self.parent.Evaluate("GUI")
         for currentGeneration in range(c.numberOfGenerations):
             self.Evolve_For_One_Generation()
-            pass
+        self.Show_Best()
 
     def Evolve_For_One_Generation(self):
         self.Spawn()
         self.Mutate()
-        self.child.Evaluate()
+        self.child.Evaluate("DIRECT")
         self.Print()
         self.Select()
 
     def Print(self):
-        print("Parent's Fitness: " + str(self.parent.fitness) + "Child's Fitness: " + str(self.child.fitness))
+        print(" Parent's Fitness: " + str(self.parent.fitness) + " Child's Fitness: " + str(self.child.fitness))
 
     def Spawn(self):
         self.child = copy.deepcopy(self.parent)
@@ -33,3 +34,6 @@ class HILL_CLIMBER:
     def Select(self):
         if(self.parent.fitness > self.child.fitness):
             self.parent = self.child
+
+    def Show_Best(self):
+        os.system("python3 simulate.py GUI")
--- simulation.py
+++ simulation.py
@@ -6,8 +6,12 @@ import robot
 
 
 class SIMULATION:
-    def __init__(self):
-        self.physicsClient = p.connect(p.DIRECT)
+    def __init__(self, directOrGUI):
+        if directOrGUI == "DIRECT":
+            self.physicsClient = p.connect(p.DIRECT)
+        else:
+            self.physicsClient = p.connect(p.GUI)
+
         p.setAdditionalSearchPath(pybullet_data.getDataPath())
         p.setGravity(0, 0, -9.8)
 
@@ -18,7 +22,7 @@ class SIMULATION:
     def run(self):
         for i in range(1000):
             #print(i)
-            #time.sleep(1 / 240)
+            time.sleep(1 / 2400)
             p.stepSimulation()
             self.robot.Sense(i)
             self.robot.Think()
